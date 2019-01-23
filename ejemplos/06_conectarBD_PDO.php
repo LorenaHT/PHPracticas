218940d252f2fc51c7474803a434f847853a1eee
LorenaHT <lore.95@outlook.com>
2019-01-23 12:20:14 +0000

    Clase (23/01/2019)

---
+++ ejemplos/06_conectarBD_PDO.php
@@ -0,0 +1,47 @@
+<section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+    <div class="my-auto">
+        <h1 class="mb-0">Conexión a Base de datos
+
+        </h1>
+        <div class="subheading mb-5">
+            <?php
+            try{
+            $servername = "localhost";
+            $username = "root";
+            $password = "elrincon";
+            $database="tienda";
+            // Create connection
+            $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $categoria = 1;
+            $sentencia = $conn->prepare("select* from books where category_id = :xyz");
+            $sentencia->bindParam(':xyz', $categoria);
+            //$sql = "select * from books where category_id=".$categoria;
+            $sentencia->execute();
+            //$stmt = $conn->query($sql);
+                $results = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+           // $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "<ul>";
+            foreach ($results as $k => $v) {
+                echo "<li>";
+                foreach ($v as $k1 => $v1)
+                    echo $k1 . " => " . $v1 . " ; ";
+                echo "</li>";
+            }
+            echo "</ul>";
+            }catch (PDOException $ex){
+                echo $ex->getMessage();
+            }catch (Exception $ex){
+                echo $ex->getMessage();
+            } finally {
+                echo "Me ejecuto SIEMPRE";
+                $conn = null;
+            }
+            ?>
+        </div>
+    </div>
+</section>
+
+
+
--- ejemplos/09_contador.php
+++ ejemplos/09_contador.php
@@ -11,24 +11,25 @@ session_start();
     session_destroy();
     session_start();
 }*/
-$contador = 0;
-if(!isset($_SESSION['contador']))
-    $_SESSION['contador']=0;
+//$contador = 0;
+
 if(isset($_POST['action']) && $_POST['action'] == "Destroy") {
     session_destroy();
     session_id(uniqid());
     session_start();
-} else if(isset($_POST['action']) && $_POST['action'] == "Sumar"){
-    $_SESSION['contador']++;
-    $contador = $_SESSION['contador'];
+}else if(isset($_POST['action']) && $_POST['action'] == "Sumar"){
+    //$_SESSION['contador']++;
+   ++$_SESSION['contador'];
 }
+if(!isset($_SESSION['contador']))
+    $_SESSION['contador']=0;
 echo session_id() . "<br/>";
 
 ?>
 <form action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
     <input type="submit" name="action" value="Sumar"/>
     <input type="submit" name="action" value="Destroy"/>
-    <input type="text" name="contador" value="<?=$contador?>"/>
+    <input type="text" name="contador" value="<?=$_SESSION['contador']?>"/>
     <!--<input type="submit" name="reiniciar" value="Reiniciar Contador"/>-->
 </form>
 <?php
