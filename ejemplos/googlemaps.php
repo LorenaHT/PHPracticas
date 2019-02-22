de2505b3ed004e57eecf2bfeff2bb0e88ba0b13f
LorenaHT <lore.95@outlook.com>
2019-02-22 18:02:44 +0000

    Tarea_29Enero2019 Ejercicio 4

--- ejemplos/googlemaps.php
+++ ejemplos/googlemaps.php
@@ -1,36 +1,41 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Simple Map</title>
-    <meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
-    <style>
-        /* Always set the map height explicitly to define the size of the div
-         * element that contains the map. */
-        #map {
-            height: 100%;
-        }
-        /* Optional: Makes the sample page fill the window. */
-        html, body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-        }
-    </style>
-</head>
-<body>
-<div id="map"></div>
-<script>
-    var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 28.123618 , lng: -15.432873},
-            zoom: 12
-        });
 
-    }
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key= AIzaSyD66tVEwouXQH78x2PsnKLl8srIlRcMMuA&callback=initMap"
-        async defer></script>
+<html>
+
+<head> <title>Simple Map</title>
+
+    <meta charset="utf-8"><title>Curso JavaScript aprenderaprogramar.com</title>
+
+    <style> *{font-family:sans-serif;}
+
+        h1 {margin: 150px 0 30px 0; text-align:center; }
+
+        #lienzoMapa {width:800px; height:600px; margin: 0 auto; border: 5px solid; }
+
+    </style>
+
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+    <script>
+        var map;
+        function initialize() {
+            var mapOptions = {
+                zoom: 4,
+                center: new google.maps.LatLng(20.54 , -10.66)
+            };
+            map = new google.maps.Map(document.getElementById('lienzoMapa'), mapOptions);
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+
+</head>
+
+<body>
+
+<h1>Utiliza el mapa</h1>
+
+<div id="lienzoMapa" ></div>
+
 </body>
+
 </html>
